9b97f4f6197536198caeeb5bbc1a1c135d56462a
benjieqiang <765126548@qq.com>
2023-05-15 21:13:29 +0800

    手写myspring

---
+++ myspring/src/test/java/com/ben/myspring/BeanTest.java
@@ -0,0 +1,15 @@
+package com.ben.myspring;
+
+/**
+ * @Author: benjieqiang
+ * @CreateTime: 2023-05-15  20:37
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class BeanTest {
+
+    public void beanTest() {
+
+    }
+
+}
